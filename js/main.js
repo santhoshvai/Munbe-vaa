20b74591b69188746a4e581aa3ba468f0e22826e
santhosh <santhoshvai@gmail.com>
2015-02-23 23:50:14 +0100

    Polished look

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var main = function () {
   var playIcon = '\u25B6 ';
   var playBtn  = $('.playBtn');
   var munbeVaaElem = $('.la-anim-12');
+  var shareElem = $('.share-container');
   var inProgress = false;
   var soundID = "Amazon-Sample";
   // load sound
@@ -9,20 +10,64 @@ var main = function () {
   function playSound () {
     createjs.Sound.play(soundID);
   }
+  // https://jonsuh.com/blog/detect-the-end-of-css-animations-and-transitions-with-javascript/
+  function whichAnimationEvent(){
+      var t,
+          el = document.createElement("fakeelement");
+
+      var animations = {
+        "animation"      : "animationend",
+        "OAnimation"     : "oAnimationEnd",
+        "MozAnimation"   : "animationend",
+        "WebkitAnimation": "webkitAnimationEnd"
+      }
+
+      for (t in animations){
+        if (el.style[t] !== undefined){
+          return animations[t];
+        }
+      }
+    }
+
+    var animationEvent = whichAnimationEvent();
+
+    function whichTransitionEvent(){
+      var t,
+          el = document.createElement("fakeelement");
+
+      var transitions = {
+        "transition"      : "transitionend",
+        "OTransition"     : "oTransitionEnd",
+        "MozTransition"   : "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+      }
+
+      for (t in transitions){
+        if (el.style[t] !== undefined){
+          return transitions[t];
+        }
+      }
+    }
+
+    var transitionEvent = whichTransitionEvent();
+  // DETECT TRANSITION END
   playBtn.click(function() {
     if( inProgress ) return false;
     document.title = playIcon + document.title; // like youtube title
     inProgress = true;
+    $('#playBtnDiv').addClass('flipOutX');
+    $("#playBtnDiv").fadeOut( "fast" );
     munbeVaaElem.addClass('la-animate');
     /* reset page after the sound has finished */
     // execute given function after the transition completes
-    munbeVaaElem.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+    munbeVaaElem.one(transitionEvent,
       function(e) {
         munbeVaaElem.removeClass( 'la-animate' );
         document.title = document.title.slice(2);
         inProgress = false;
     });
     playSound();
+    shareElem.addClass('fadeInRight'); // show the share button
     $(".language").addClass('anim lang');
     var arr = ['one', 'two', 'three', 'four', 'five'];
     for ( var i = 0; i <= 1; i+=1 ) {
@@ -31,10 +76,14 @@ var main = function () {
       var currentVerse = $(".lyric" + idx);
       var nextVerse = $(".lyric" + nxtIdx);
       currentVerse.addClass('anim ' + arr[i]);
-      currentVerse.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-            nextVerse.addClass('anim ' + arr[i+1]));
-    }
+    //   currentVerse.one(animationEvent,
+    //       function(event) {
+    //             // currentVerse.removeClass('anim ' + arr[i]);
+    //             nextVerse.addClass('anim ' + arr[i+1]);
+    //             $('.la-anim-12').append('<p>Animation complete</p>');
+    //         });
 
+     }
   });
 }
 
