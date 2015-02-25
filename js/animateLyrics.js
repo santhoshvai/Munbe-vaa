8635b13afec4911940d6b1991de8d8e329a94532
santhosh <santhoshvai@gmail.com>
2015-02-26 00:09:26 +0100

    parallel animations to sequential

--- js/animateLyrics.js
+++ js/animateLyrics.js
@@ -1,26 +1,8 @@
-function velocityFadeInUp(elem, delayV, durationV) {
-    elem.velocity({translateY: [0, 100], opacity: [1, 0]}, { delay: delayV, duration: durationV });
-}
-function velocityFade(elem, delayV, durationV) {
-    elem.velocity({ opacity: 1 }, { delay: delayV, duration: durationV });
-}
+function velocityFadeInUp(a,e,t){a.velocity({translateY:[0,100],opacity:[1,0]},{delay:e,duration:t})}function velocityFade(a,e,t){a.velocity({opacity:1},{delay:e,duration:t})}function allFades(a){var e=[{e:a[0],p:{opacity:1},o:{duration:1e3,delay:5530}},{e:a[1],p:{opacity:1},o:{duration:1e3,delay:2370}},{e:a[2],p:{opacity:1},o:{duration:1e3,delay:2230}},{e:a[3],p:{opacity:1},o:{duration:1e3,delay:2640}},{e:a[4],p:{opacity:1},o:{duration:1e3,delay:2880}},{e:a[5],p:{opacity:1},o:{duration:1e3,delay:5560}},{e:a[6],p:{opacity:1},o:{duration:1e3,delay:2110}}];$.Velocity.RunSequence(e)}function allFadeIns(a){var e=[{e:a[0],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:5530}},{e:a[1],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:2370}},{e:a[2],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:2230}},{e:a[3],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:2640}},{e:a[4],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:2880}},{e:a[5],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:5560}},{e:a[6],p:{translateY:[0,100],opacity:[1,0]},o:{duration:1e3,delay:2110}}];$.Velocity.RunSequence(e)}
+
 function animateLyricsAfterPlay(arr, isMobile) {
     $(".language").velocity({ opacity: 1}, "slow");
-    if(isMobile) {
-        velocityFade(arr[0], 5600, 1000);
-        velocityFade(arr[1], 8300, 1000);
-        velocityFade(arr[2], 11700, 1000);
-        velocityFade(arr[3], 15500, 1000);
-        velocityFade(arr[4], 18500, 1000);
-        velocityFade(arr[5], 26100, 1000);
-        velocityFade(arr[6], 30000, 1000);
-        return;
-    }
-    velocityFadeInUp(arr[0], 5600, 1000);
-    velocityFadeInUp(arr[1], 8300, 1000);
-    velocityFadeInUp(arr[2], 11700, 1000);
-    velocityFadeInUp(arr[3], 15500, 1000);
-    velocityFadeInUp(arr[4], 18500, 1000);
-    velocityFadeInUp(arr[5], 26100, 1000);
-    velocityFadeInUp(arr[6], 30000, 1000);
+    if (isMobile) allFades(arr);
+    else allFadeIns(arr);
+    return;
 }
