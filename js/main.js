6d5cb3a2afaa6aa2e51977527634b7e6ebb6dca4
santhosh <santhoshvai@gmail.com>
2015-02-26 14:03:41 +0100

    added webm file now, firefox prefers this over ogg due to more compression

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ var main = function () {
     }
     var audio =  new Howl({
         urls: ['https://cdn.rawgit.com/santhoshvai/Munbe-vaa/gh-pages/Media/Amazon-Sample.mp3',
+        'https://cdn.rawgit.com/santhoshvai/Munbe-vaa/gh-pages/Media/Amazon-Sample.webm',
          'https://cdn.rawgit.com/santhoshvai/Munbe-vaa/gh-pages/Media/Amazon-Sample.ogg'],
          // 'https://cdn.rawgit.com/santhoshvai/Munbe-vaa/gh-pages/Media/Amazon-Sample.wav'],
         onload: function () {
